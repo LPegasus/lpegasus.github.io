f087b866b3ee0ac8aec6e4e6a88bdf4a8bfb36d5
Pegasusknight <pegusas1@hotmail.com>
2018-03-17 11:59:23 +0800

    'update'

---
+++ src/entry/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class Index extends React.Component<any, any> {
+  render() {
+    return (
+
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.querySelector('#root'));
